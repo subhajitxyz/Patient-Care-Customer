1078a102835d010215fc2c0c0b0779ca65ca3f6c
subhajitxyz <subhajitmallick879@gmail.com>
2026-03-16 10:13:24 +0530

    notification feature

--- app/src/main/java/com/real/patientcare/notification/HealthFirebaseMessagingService.kt
+++ app/src/main/java/com/real/patientcare/notification/HealthFirebaseMessagingService.kt
@@ -7,7 +7,6 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
